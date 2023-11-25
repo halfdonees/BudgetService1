f28ea0a8619f5c806dbc22b52bc06267bf12c067
KuanTsen <kuantsen.lu@titansoft.com.sg>
2023-11-25 17:19:55 +0800

    two days test

---
+++ TestProject1/Budget.cs
@@ -0,0 +1,7 @@
+﻿namespace TestProject1;
+
+public class Budget
+{
+    public string YearMonth { get; set; }
+    public int Amount { get; set; }
+}
--- TestProject1/BudgetService.cs
+++ TestProject1/BudgetService.cs
@@ -2,14 +2,39 @@
 
 public class BudgetService
 {
+    private readonly IBudgetRepo _repo;
+
+    public BudgetService(IBudgetRepo repo)
+    {
+        _repo = repo;
+    }
+
+
     public decimal Query(DateTime start, DateTime end)
     {
         if (end >= start)
         {
+            var allBudget = _repo.GetAll();
+            var fullMonthBudget = allBudget.FirstOrDefault(x => x.YearMonth == $"{end.Year}{end.Month}", new Budget()
+            {
+            });
+
+            var dailyBudget = fullMonthBudget.Amount / GetDaysInMonth(end);
             var days = (end - start).Days + 1;
-            return 10 * days;
+
+            return dailyBudget * days;
         }
 
         return 0;
     }
+
+    private int GetDaysInMonth(DateTime end)
+    {
+        return DateTime.DaysInMonth(end.Year, end.Month);
+    }
+}
+
+public interface IBudgetRepo
+{
+    public List<Budget> GetAll();
 }
