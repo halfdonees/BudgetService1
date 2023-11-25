08f928eca06cf3c63e426c1c8108ac080033e570
KuanTsen <kuantsen.lu@titansoft.com.sg>
2023-11-25 16:41:51 +0800

    2 days case

--- TestProject1/BudgetService.cs
+++ TestProject1/BudgetService.cs
@@ -4,6 +4,12 @@ public class BudgetService
 {
     public decimal Query(DateTime start, DateTime end)
     {
+        if (end >= start)
+        {
+            var days = (end - start).Days + 1;
+            return 10 * days;
+        }
+
         return 0;
     }
 }
